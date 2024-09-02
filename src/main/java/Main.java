71f19d75505746b2dd0289adfba7dd68b578411b
José Gabriel Morais <169068802+moraisdino@users.noreply.github.com>
2024-09-02 12:36:34 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+  Scanner sc = new Scanner (System.in);
+
+  System.out.println("digite seu primeiro nome");   String nome = sc.next();
+    
+    System.out.println("digite seu sobrenome");
+    String sobrenome = sc.next();
+
+System.out.println("seu nome completo é: " + nome + "  " + sobrenome );
+    sc.close();
+  }
+
+}
